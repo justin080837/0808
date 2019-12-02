d5dd9030ac68df271e39c2f6b81b07d4a184189e
justin <justin080837@gmail.com>
2019-12-02 20:14:40 +0800

    creat a new py

---
+++ 1028.py
@@ -0,0 +1,20 @@
+'''
+def hello ():
+    print('a')
+hello()
+'''
+n='xS'
+if(type(n)==int):
+    print('整數')
+
+elif(type(n)==float):
+    print('浮點數')
+
+else:
+    print('都不是')
+var=[1,2,3,4,5]
+print(var[1:-1:2])
+#[起點,終點,跳步]
+
+a = '131545498'
+print(len(a))
